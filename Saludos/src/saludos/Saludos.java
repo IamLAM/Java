01aabccec4778288d0681b16e02f117010f8283e
IamLAM <ingeniero@luismonge.com.mx>
2018-05-18 20:06:37 -0700

    Saludo
    Ejercicio básico que imprime una en pantalla un mensaje

---
+++ Saludos/src/saludos/Saludos.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package saludos;
+
+/**
+ *
+ * @author Luis Monge
+ */
+public class Saludos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Bienvenido al curso de Java");
+    }
+    
+}
