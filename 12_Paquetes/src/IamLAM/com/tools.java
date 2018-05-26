4aaaeefcbeaedb606aa1ffb098b5e41f55a286ae
IamLAM <ingeniero@luismonge.com.mx>
2018-05-26 13:42:21 -0700

    Paquetes
    Creación de un paquete y uso en la clase principal

---
+++ 12_Paquetes/src/IamLAM/com/tools.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IamLAM.com;
+
+/**
+ *
+ * @author Luis Monge
+ */
+public class tools {
+    
+    public static void desplegar(){
+    
+    System.out.println("Mensaje desde tools paqueteria");
+    }
+    
+}
