f71832c6bb8c550f33a011bef6795060dea3cfda
IamLAM <ingeniero@luismonge.com.mx>
2018-07-06 21:58:29 -0700

    Codificación de la clase conexión

--- 35_ManejoPersonal/src/datos/Conexion.java
+++ 35_ManejoPersonal/src/datos/Conexion.java
@@ -5,10 +5,70 @@
  */
 package datos;
 
+import java.sql.*;
+
 /**
  *
  * @author Luis Monge
  */
 public class Conexion {
-    
+
+    private static String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private static String JDBC_URL = "jdbc:mysql://localhost/sqa?useSSL=false";
+    private static String JDBC_PASS = "";
+    private static String JDBC_USER = "root";
+    private static Driver driver = null;
+
+    public static synchronized Connection getConnection() throws SQLException {
+        /*Para trabajar con un hilo a la vez*/
+        if (driver == null) {
+
+            try {
+
+                Class jdbcDriverClass = Class.forName(JDBC_DRIVER);
+                driver = (Driver) jdbcDriverClass.newInstance();
+                DriverManager.deregisterDriver(driver);
+            } catch (Exception e) {
+                System.out.println("Fallo la carga del driver JDBC");
+                e.printStackTrace();
+            }
+        }
+
+        return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_URL);
+    }
+
+    public static void close(ResultSet rs) {
+
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+
+    }
+
+    public static void close(PreparedStatement ps) {
+
+        try {
+            if (ps != null) {
+                ps.close();
+            }
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+
+    }
+
+    public static void close(Connection con) {
+
+        try {
+            if (con != null) {
+                con.close();
+            }
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+    }
 }
